07f977fd14db09a5081f4f226f7c0208d2ebef4a
Ryan Niño Dizon <ryan.dizon@isdfeniqs.com>
2024-05-07 02:03:24 +0200

    Updated README

--- msal-angular17-standalone-with-listdata/src/environments/environment.e2e.ts
+++ msal-angular17-standalone-with-listdata/src/environments/environment.e2e.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     msalConfig: {
         auth: {
-            clientId: '3fba556e-5d4a-48e3-8e1a-fd57c12cb82e',
-            authority: 'https://login.windows-ppe.net/common'
+            clientId: '<client-id>',
+            authority: 'https://login.microsoftonline.com/<tenant-id>'
         }
     },
     apiConfig: {
