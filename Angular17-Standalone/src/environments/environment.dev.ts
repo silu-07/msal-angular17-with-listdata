4157abbfacf350edc7e9e312b46b67322cb86e24
Ryan Niño Dizon <50763950+ryanninodizon@users.noreply.github.com>
2024-07-13 21:06:48 +0200

    Update environment.dev.ts

--- Angular17-Standalone/src/environments/environment.dev.ts
+++ Angular17-Standalone/src/environments/environment.dev.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     msalConfig: {
         auth: {
-            clientId: 'c1eed0b4-a59d-46bd-b3ee-416e35abbfa3',
-            authority: 'https://login.microsoftonline.com/3e11228a-56f5-450d-9cf1-6283bdb7f12c'
+            clientId: 'c1eed0b4-a59d-46bd-b3ee-416e35abzfa3',
+            authority: 'https://login.microsoftonline.com/3e11228b-56f5-450d-9cf1-6283bdb7f12c'
         }
     },
     apiConfig: {
